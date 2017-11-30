33b547c2b758a18b1f119594df67d5be1075ef86
sagarsrao <sagar.s.rao@gmail.com>
2017-11-30 18:08:53 +0530

    Fixed all the changes suggested by the reviewer

--- app/src/main/java/com/udacity/popmovies/activities/MovieActivity.java
+++ app/src/main/java/com/udacity/popmovies/activities/MovieActivity.java
@@ -131,7 +131,6 @@ public class MovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_movies);
@@ -148,14 +147,13 @@ public class MovieActivity extends AppCompatActivity {
         mFavoriteMovieAdapter = new FavoriteMovieAdapter(MovieActivity.this, favoriteMovieList, favoriteToggleStatuses);//favoriteMoviesListFromTheContentProvider
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setAdapter(mFavoriteMovieAdapter);
-
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState != null) {
-
+            id = savedInstanceState.getInt("last_category_selected");
         }
     }
 
@@ -171,134 +169,138 @@ public class MovieActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
-
+        outState.putInt("last_category_selected", id);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         id = item.getItemId();
-
-        if (id == R.id.action_most_popular) {
-            Log.d(TAG, "onOptionsItemSelected:Most_popularMovies" + id);
-            Toast.makeText(this, getString(R.string.toast_pop_movies_clicked) + id, Toast.LENGTH_SHORT).show();
-            /*Make an retrofit API networking operations here*/
-
-            RetrofitApiEndPoints apiEndPoints = RetrofitClient.getClient().create(RetrofitApiEndPoints.class);
-
-            Call<ResponseMovie> call = apiEndPoints.getTopPopularMovies(BuildConfig.API_KEY);
-            call.enqueue(new Callback<ResponseMovie>() {
-                @Override
-                public void onResponse(Call<ResponseMovie> call, Response<ResponseMovie> response) {
-
-                    if (response != null) {
-
-                        Log.d(TAG, "onResponse: " + response.body().getResults());
-                        int statusCode = response.code();
-                        Log.d(TAG, "onResponse: " + statusCode);
-                        movie = response.body().getResults();
-
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mAdapter = new MovieAdapter(MyApplication.getAppContext(), movie, clickListener);
-
-                        mRecyclerView.setAdapter(mAdapter);
-                        mAdapter.notifyDataSetChanged();
-
-                    }
-
-                }
-
-                @Override
-                public void onFailure(Call<ResponseMovie> call, Throwable t) {
-
-                    Toast.makeText(MovieActivity.this, "Failure to load the data", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-
-            return true;
-        }
-
-        if (id == R.id.action_top_rated) {
-
-            Log.d(TAG, "onOptionsItemSelected:topRated" + id);
-
-            Toast.makeText(this, getString(R.string.toast_top_rated_movies_clicked) + id, Toast.LENGTH_SHORT).show();
-            RetrofitApiEndPoints apiEndPoints = RetrofitClient.getClient().create(RetrofitApiEndPoints.class);
-
-            Call<ResponseMovie> call = apiEndPoints.getTopRatedMovies(BuildConfig.API_KEY);
-            call.enqueue(new Callback<ResponseMovie>() {
-                @Override
-                public void onResponse(Call<ResponseMovie> call, Response<ResponseMovie> response) {
-
-                    if (response != null) {
-
-                        Log.d(TAG, "onResponse: " + response.body().getResults());
-                        int statusCode = response.code();
-                        Log.d(TAG, "onResponse: " + statusCode);
-                        movie = response.body().getResults();
-
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mAdapter = new MovieAdapter(MyApplication.getAppContext(), movie, clickListener);
-
-                        mRecyclerView.setAdapter(mAdapter);
-                        /*This will help us in refreshing the whole adapter*/
-                        mAdapter.notifyDataSetChanged();
-                    }
-
-                }
-
-                @Override
-                public void onFailure(Call<ResponseMovie> call, Throwable t) {
-
-                    Toast.makeText(MovieActivity.this, "Failure to load the data", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-
-            return true;
-        }
-        if (id == R.id.action_favorite) {
-            Log.d(TAG, "onOptionsItemSelected:fav_Movies" + id);
-
-            Toast.makeText(this, "You clicked on favorite option!!!!!!!!" + id, Toast.LENGTH_SHORT).show();
-
-
-            mCursor = getContentResolver().query(MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
-            if (null != mCursor && mCursor.getCount() > 0) {
-                mCursor.moveToFirst();
-                if (mCursor.moveToFirst()) {
-                    do {
-
-                        favoriteMovieList.add(mCursor.getString(mCursor.getColumnIndexOrThrow(MovieContract.MovieEntry.MOVIE_TITLE)));
-                        byte[] favoriteMovie = mCursor.getBlob(mCursor.getColumnIndexOrThrow(MovieContract.MovieEntry.MOVIE_IMAGE));
-                        Bitmap newFavoriteMovie = DbBitmapUtility.convertToBitmap(favoriteMovie);
-                        favoriteToggleStatuses.add(newFavoriteMovie);
-
-
-                    } while (mCursor.moveToNext());
-                    mCursor.close();
-
-                }
-
-
-                mRecyclerView.setLayoutManager(layoutManager);
-
-                mFavoriteMovieAdapter = new FavoriteMovieAdapter(MovieActivity.this, favoriteMovieList, favoriteToggleStatuses);//favoriteMoviesListFromTheContentProvider
-
-                mRecyclerView.setAdapter(mFavoriteMovieAdapter);
-
-                mFavoriteMovieAdapter.notifyDataSetChanged();
-            }
-
-
-        }
-
-
-        return super.onOptionsItemSelected(item);
+        return populateMovies();
     }
 
+    private boolean populateMovies() {
+
+        if (id == 0)
+            id = R.id.action_most_popular;
+
+        switch (id) {
+            case R.id.action_most_popular: {
+                Log.d(TAG, "onOptionsItemSelected:Most_popularMovies" + id);
+                Toast.makeText(this, getString(R.string.toast_pop_movies_clicked) + id, Toast.LENGTH_SHORT).show();
+            /*Make an retrofit API networking operations here*/
+
+                RetrofitApiEndPoints apiEndPoints = RetrofitClient.getClient().create(RetrofitApiEndPoints.class);
+
+                Call<ResponseMovie> call = apiEndPoints.getTopPopularMovies(BuildConfig.API_KEY);
+                call.enqueue(new Callback<ResponseMovie>() {
+                    @Override
+                    public void onResponse(Call<ResponseMovie> call, Response<ResponseMovie> response) {
+
+                        if (response != null) {
+
+                            Log.d(TAG, "onResponse: " + response.body().getResults());
+                            int statusCode = response.code();
+                            Log.d(TAG, "onResponse: " + statusCode);
+                            movie = response.body().getResults();
+
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mAdapter = new MovieAdapter(MyApplication.getAppContext(), movie, clickListener);
+
+                            mRecyclerView.setAdapter(mAdapter);
+                            mAdapter.notifyDataSetChanged();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseMovie> call, Throwable t) {
+
+                        Toast.makeText(MovieActivity.this, "Failure to load the data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+                return true;
+            }
+            case R.id.action_top_rated: {
+
+                Log.d(TAG, "onOptionsItemSelected:topRated" + id);
+
+                Toast.makeText(this, getString(R.string.toast_top_rated_movies_clicked) + id, Toast.LENGTH_SHORT).show();
+                RetrofitApiEndPoints apiEndPoints = RetrofitClient.getClient().create(RetrofitApiEndPoints.class);
+
+                Call<ResponseMovie> call = apiEndPoints.getTopRatedMovies(BuildConfig.API_KEY);
+                call.enqueue(new Callback<ResponseMovie>() {
+                    @Override
+                    public void onResponse(Call<ResponseMovie> call, Response<ResponseMovie> response) {
+
+                        if (response != null) {
+
+                            Log.d(TAG, "onResponse: " + response.body().getResults());
+                            int statusCode = response.code();
+                            Log.d(TAG, "onResponse: " + statusCode);
+                            movie = response.body().getResults();
+
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mAdapter = new MovieAdapter(MyApplication.getAppContext(), movie, clickListener);
+
+                            mRecyclerView.setAdapter(mAdapter);
+                        /*This will help us in refreshing the whole adapter*/
+                            mAdapter.notifyDataSetChanged();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseMovie> call, Throwable t) {
+
+                        Toast.makeText(MovieActivity.this, "Failure to load the data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+                return true;
+            }
+            case R.id.action_favorite:
+                Log.d(TAG, "onOptionsItemSelected:fav_Movies" + id);
+
+                Toast.makeText(this, "You clicked on favorite option!!!!!!!!" + id, Toast.LENGTH_SHORT).show();
+
+
+                mCursor = getContentResolver().query(MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
+                if (null != mCursor && mCursor.getCount() > 0) {
+                    mCursor.moveToFirst();
+                    if (mCursor.moveToFirst()) {
+                        do {
+
+                            favoriteMovieList.add(mCursor.getString(mCursor.getColumnIndexOrThrow(MovieContract.MovieEntry.MOVIE_TITLE)));
+                            byte[] favoriteMovie = mCursor.getBlob(mCursor.getColumnIndexOrThrow(MovieContract.MovieEntry.MOVIE_IMAGE));
+                            Bitmap newFavoriteMovie = DbBitmapUtility.convertToBitmap(favoriteMovie);
+                            favoriteToggleStatuses.add(newFavoriteMovie);
+
+
+                        } while (mCursor.moveToNext());
+                        mCursor.close();
+
+                    }
+
+
+                    mRecyclerView.setLayoutManager(layoutManager);
+
+                    mFavoriteMovieAdapter = new FavoriteMovieAdapter(MovieActivity.this, favoriteMovieList, favoriteToggleStatuses);//favoriteMoviesListFromTheContentProvider
+
+                    mRecyclerView.setAdapter(mFavoriteMovieAdapter);
+
+                    mFavoriteMovieAdapter.notifyDataSetChanged();
+                }
+
+
+                break;
+        }
+        return false;
+    }
 
     private int numberOfColumns() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -319,7 +321,7 @@ public class MovieActivity extends AppCompatActivity {
             Parcelable listState = mBundleRecyclerViewState.getParcelable(KEY_RECYCLER_STATE);
             mRecyclerView.getLayoutManager().onRestoreInstanceState(listState);
         }
-
+        populateMovies();
 
     }
 
